e17103d3b4aac5cacf6ea2ba072afa5f46f65b16
leo <lichaofeng1991@gmail.com>
2019-05-09 18:28:22 +0800

    第一次提交

---
+++ src/main/java/com/ssm/controller/chapter2/HelloWordController.java
@@ -0,0 +1,18 @@
+package com.ssm.controller.chapter2;
+
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWordController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "Hello World!");
+        mv.setViewName("hello");
+        return mv;
+    }
+}
